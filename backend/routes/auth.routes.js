9b6a5054cf2bdcc0aa2cac4e55a7008dd2a973ac
Divyanshu Singh <divyanshusingh12082003@gmail.com>
2024-03-05 15:09:43 +0530

    succesfully done login and logout

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,13 +1,36 @@
 import express from "express";
 import User from "../models/user.model.js";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
+
+import bcrypt from "bcryptjs";
 
 const router = express.Router();
 
 // Use express.json() middleware to parse JSON bodies
 router.use(express.json());
 
-router.post("/login", (req, res) => {
-    res.send("Login Route");
+router.post("/login", async(req, res) => {
+    try {
+		const { username, password } = req.body;
+		const user = await User.findOne({ username });
+		const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+
+		if (!user || !isPasswordCorrect) {
+			return res.status(400).json({ error: "Invalid username or password" });
+		}
+
+		generateTokenAndSetCookie(user._id, res);
+
+		res.status(200).json({
+			_id: user._id,
+			fullName: user.fullName,
+			username: user.username,
+			profilePic: user.profilePic,
+		});
+	} catch (error) {
+		console.log("Error in login controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
 });
 
 router.post("/signup", async (req, res) => {
@@ -23,8 +46,12 @@ router.post("/signup", async (req, res) => {
         if (user) {
             return res.status(400).json({ error: "Username already exists" });
         }
-
         //HASH PASSWORD HERE
+
+        const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(password, salt);
+
+        
 
         const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
         const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
@@ -32,12 +59,13 @@ router.post("/signup", async (req, res) => {
         const newUser = new User({
             fullName,
             username,
-            password,
+            password:hashedPassword,
             gender,
             profilePic: gender === "male" ? boyProfilePic : girlProfilePic
         });
-
-        await newUser.save();
+        if(newUser){
+            generateTokenAndSetCookie(newUser._id,res);
+            await newUser.save();
 
         res.status(201).json({
             _id: newUser._id,
@@ -45,14 +73,25 @@ router.post("/signup", async (req, res) => {
             username: newUser.username,
             profilePic: newUser.profilePic,
         });
+        }
+        else{
+            res.status(400).json({error:"Invalid User data"});
+        }
+        
     } catch (error) {
         console.error("Error in signup ", error.message);
         res.status(500).json({ error: "Internal server Error" });
     }
 });
 
-router.post("/signout", (req, res) => {
-    res.send("Signout Route");
+router.post("/logout", async(req, res) => {
+    try {
+		res.cookie("jwt", "", { maxAge: 0 });
+		res.status(200).json({ message: "Logged out successfully" });
+	} catch (error) {
+		console.log("Error in logout controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
 });
 
 export default router;
