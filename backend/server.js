64783f6d6920dde74463ca4defacfa320c4bdfc5
Divyanshu Singh <divyanshusingh12082003@gmail.com>
2024-03-09 21:22:16 +0530

    socket done for messages

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,10 @@ import userRoutes from "./routes/user.routes.js";
 import cookieParser from "cookie-parser";
 
 import connectToMongo from "./db/connectToMongo.js";
-
+import { app, server } from "./socket/socket.js";
 dotenv.config();
 
-const app=express();
+
 
 const PORT=process.env.PORT || 5000;
 
@@ -41,7 +41,7 @@ app.use("/api/users",userRoutes);
 
 
 
-app.listen(PORT,()=>{
+server.listen(PORT,()=>{
     connectToMongo();
     console.log("I am here");
     console.log(`Listening...${PORT}`);
