6f7533927928d285cb232b657c5550cdb44f1f57
Divyanshu Singh <divyanshusingh12082003@gmail.com>
2024-03-09 22:01:50 +0530

    deploymrnt

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv"
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
-
+import path from "path"
 import cookieParser from "cookie-parser";
 
 import connectToMongo from "./db/connectToMongo.js";
@@ -11,7 +11,7 @@ import { app, server } from "./socket/socket.js";
 dotenv.config();
 
 
-
+const __dirname = path.resolve()
 const PORT=process.env.PORT || 5000;
 
 
@@ -32,6 +32,13 @@ app.use(cookieParser());
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 
 //to parse requests from user body
 
